cdadbc3017387232ed28cea0aae7b70085620a0e
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2021-03-08 21:32:38 -0300

    Adicionando os comentarios.

--- utils/dijkstra.py
+++ utils/dijkstra.py
@@ -17,6 +17,9 @@ class optimizer:
 
 
 	def timePassed(self, node):
+		"""
+		Calcula quanto tempo se passou desde o node que foi passado para a função.
+		"""
 		tmpNode = node
 		totalInstances = 1
 
@@ -30,26 +33,32 @@ class optimizer:
 
 	def resolveWeights(self, baseNode, newNode):
 		"""
-		Função que 
+		Função que muda os valores de cada node para cada novo conjunto de nodes.
 		"""
 
-		if baseNode.name == "Comer" and newNode.name == "Comer":
+		if baseNode.name == "Comer" and newNode.name == "Comer": # Caso de comer grafico 2
 			newNode.value = baseNode.value + 10000
-		# (e-1)x-e+2\ 
+
 		elif newNode.name == "Comer" :
+
 			if self.needs.completed[newNode.type] >= self.needs.total[newNode.type]:
+
 				newNode.value = math.inf
 				self.needs.resetCompleted()
-			elif newNode.value * (((math.e-1)*self.sameTaksB4(newNode))  - math.e + 2) > 9999:
+			# ((e-1) * x ) - (e + 2)
+			elif newNode.value * (((math.e-1)*self.sameTaksB4(newNode))  - math.e + 2) > 9999: #Caso o crescimento seja muito para calcular e não ficar um numero muito pequeno.
 				newNode.value = 0
 			else:
 				newNode.subValue(((math.e-1)*self.sameTaksB4(newNode))  - math.e + 2)
-		else:
+
+
+		else: # Caso de trabalhos, entreterimento e outros grafico 1
 			if self.needs.completed[newNode.type] >= self.needs.total[newNode.type]:
 				newNode.value = math.inf
 				self.needs.resetCompleted()
 
 			elif newNode.type is self.needs.getPriority():
+				# x^(e) - x + 1
 				newNode.subValue(newNode.value * ((self.sameTaksB4(newNode)**math.e) - self.sameTaksB4(newNode) + 1))
 				self.needs.resetCompleted()
 
@@ -70,7 +79,9 @@ class optimizer:
 
 
 	def countTasksDone(self, node):
-		
+		"""
+		Função para calcular quanto de cada tipo de tarefa foi feito.
+		"""
 		tmpNode = node
 		valueToSum = 1
 		while self.distances[tmpNode][0].name != "Acordar":
@@ -80,21 +91,24 @@ class optimizer:
 
 
 	def createNextNodeLine(self, baseNode):
+			"""
+			Função para criar o proximo conjunto de tarefas.
+			"""
 			newDistances = []
 
-			if self.timePassed(baseNode) >= self.needs.dayTotalTime +2 or not baseNode:
+			if self.timePassed(baseNode) >= self.needs.dayTotalTime + 2 or not baseNode: # Caso se tenha passado o total de tempo total, retornar para criar o node de dormir.
 				return False			
 			
 			for tmpNode in self.taskList:
 
 				newNode = TKS(tmpNode.name, tmpNode.type, tmpNode.value, False)
 
-				self.distances.update({newNode: [baseNode, None]})
+				self.distances.update({newNode: [baseNode, None]}) # Adicionando o novo node nas distancias, pois ele é um novo node é não pode ser visitado 2x.
 
-				self.countTasksDone(newNode)
-				self.resolveWeights(baseNode, newNode)
+				self.countTasksDone(newNode) # Contando quantas tarefas foram feitas para o resolveWeights.
+				self.resolveWeights(baseNode, newNode) # Calcula o peso do novo node.
 
-				self.distances.update({newNode: [baseNode, newNode.value + baseNode.value]})
+				self.distances.update({newNode: [baseNode, newNode.value + baseNode.value]}) # atualiza a distancia do novo node.
 
 				newDistances.append((newNode, baseNode, newNode.value))
 
@@ -103,7 +117,7 @@ class optimizer:
 
 	def dijkstra(self):
 
-		totalChoices = 500
+		totalChoices = 500 # Total de caminhos aceitos.
 		priorityQueue = []
 		self.distances.update({self.startNode: [self.startNode, 0]})
 		
@@ -119,19 +133,22 @@ class optimizer:
 			# print(self.timePassed(current[0]))
 			newNodes = self.createNextNodeLine(current[0])
 			
-			if not newNodes:
+			if not newNodes: # Quando se chega no ultimo node.
 
-				if self.endNode not in self.distances:
+				if self.endNode not in self.distances: # Caso ele não esteja nas distancias.
 					self.distances.update({self.endNode: [current[0], current[0].value]})
-				elif self.distances.get(self.endNode)[1] > current[0].value:
+				elif self.distances.get(self.endNode)[1] > current[0].value: # Caso o novo valor seja menor que o ja cadastrado.
 					self.distances.update({self.endNode: [current[0], current[0].value]})
 			
 			else:
 				priorityQueue += newNodes
-				priorityQueue.sort(key=itemgetter(2))
-				priorityQueue = priorityQueue[0:totalChoices]
+				priorityQueue.sort(key=itemgetter(2)) # Ordenação para os melhores caminhos.
+				priorityQueue = priorityQueue[0:totalChoices] # Descarte dos piores caminhos.
 
 	def createPath(self):
+		"""
+		Constroi o caminho para o resultado do dijkstra.
+		"""
 		tmpNode = self.endNode
 		instance = self.needs.dayTotalTime + 1
 		path=[(tmpNode.name, instance)]
@@ -139,7 +156,9 @@ class optimizer:
 			tmpNode = self.distances.get(tmpNode)[0]
 			instance-=1
 			path.append((tmpNode.name, instance))
+
 		instance-=1
+
 		path.append((self.distances.get(tmpNode)[0].name, instance))
 		self.needs.resetCompleted()
 		self.countTasksDone(self.endNode)
@@ -152,5 +171,7 @@ class optimizer:
 			else:
 				postProcess.append([i[0], 1])
 				j+=1
-	
+
+		# Path - Caminho com as instancias.
+		# Postprocess - Caminho com a quantidade feita de cada um.
 		return path[::-1], postProcess, self.needs.completed, self.needs.total
--- utils/tasks.py
+++ utils/tasks.py
@@ -2,29 +2,36 @@
 class needs:
 	def __init__(self, DayTotalTime=150, work=60, entertainment=30, other=10) -> None:
 
+			# Transforma os pesos de cada coisa em uma porcentagem do dia.
 			self.work = (100 * work) / sum([work, entertainment, other])
 			self.entertainment = (100 * entertainment) / sum([work, entertainment, other])
 			self.other = (100 * other) / sum([work, entertainment, other])
 
-
+			# O total que se precisa ser feito durante o dia.
 			self.total = [DayTotalTime * (self.work / 100), DayTotalTime * (self.entertainment / 100), DayTotalTime * (self.other / 100), 2]
 
-			self.completed = [0, 0, 0, 0]
+			# Quanto se foi feito durante o dia.
+			self.completed = [0] * len(self.total)
 			
 			self.dayTotalTime = DayTotalTime
 
 	def getPriority(self):
+		"""
+		Retorna o indice da tarefa que foi menos feita.
+		"""
 		delta = [iTotal - iCompleted for iTotal, iCompleted in zip(self.total, self.completed)]
 		return delta.index(max(delta))
 
 	def resetCompleted(self):
-		self.completed = [0, 0, 0, 0]
+		self.completed = [0] * len(self.total)
 		
 	def addToCompleted(self, index, value):
 		if self.completed[index] + value > self.total[index]:
 			self.completed[index] = self.total[index]
 		else:
 			self.completed[index] += value
+
+
 class task:
 	def __init__(self, name: str,  taskType: int, taskValue:float, create: bool) -> None:
 			"""
